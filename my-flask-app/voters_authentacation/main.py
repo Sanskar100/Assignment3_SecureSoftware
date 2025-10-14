52213f43197b0f22f48649a3c7c8d16777840f7e
sanskarpokharel <sanskarpokharel10@gmail.com>
2025-10-14 16:11:03 +1100

    added login for voters

--- my-flask-app/voters_authentacation/main.py
+++ my-flask-app/voters_authentacation/main.py
@@ -18,7 +18,7 @@ REGISTER_TEMPLATE = """
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Register</title>
-    <link rel="stylesheet" href="{{ url_for('static', filename='css/style.css') }}">
+    <link rel="stylesheet" href="css/style,css">
 </head>
 <body>
     <div class="container">
@@ -42,4 +42,26 @@ REGISTER_TEMPLATE = """
 </body>
 </html>
 """
-
+Voter_Login="""
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style,css">
+</head>
+<body>
+    <div class="container">
+        <h1>Voter Login</h1>
+        <form method="POST" action="/login">
+            <label for="email">Email:</label><br>
+            <input type="email" id="email" name="email" required><br><br>
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password" required><br><br>
+            <input type="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
+"""
