a7cc9e56b06275609340f29dbacd064ad6ec4c6d
sanskarpokharel <sanskarpokharel10@gmail.com>
2025-10-16 23:25:11 +1100

    Added password validation

--- my-flask-app/voting_app/main.py
+++ my-flask-app/voting_app/main.py
@@ -6,6 +6,7 @@ import re
 import random
 
 
+
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'a_super_secret_key_for_dev') # Used for flash messages and sessions
 
@@ -287,6 +288,19 @@ def init_db():
 @app.before_first_request
 def before_first_request():
     init_db()
+
+def password_validation(password):
+    if len(password) < 8:
+        return "Password must be at least 8 characters long."
+    if not re.search(r"[A-Z]", password):
+        return "Password must contain at least one uppercase letter."
+    if not re.search(r"[a-z]", password):
+        return "Password must contain at least one lowercase letter."
+    if not re.search(r"[0-9]", password):
+        return "Password must contain at least one digit."
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        return "Password must contain at least one special character."
+    return True
 
 @app.route('/')
 def index():
